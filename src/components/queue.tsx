ad64aaf2084d3b3356c5c11a3b9664080aaa1efe
Nathan Krasney <natankrasney@gmail.com>
2023-07-19 10:53:48 +0300

    queue enqueue dequeue work nicely on the UI

--- src/components/queue.tsx
+++ src/components/queue.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import styles from "../styles/queue.module.css";
 import Task from "../types/e-task";
 import GenericEnumBasedSelect from "./gen/generic-enum-based-select";
@@ -7,16 +7,6 @@ const Queue = () => {
   const [task, setTask] = useState(Task.task1);
   const [queue, setQueue] = useState<Task[]>([]);
 
-  const reversed = [...queue].reverse();
-  const queueElem = reversed.map((it, i) => {
-    const elem = (
-      <div className={styles.queue_item} key={i}>
-        {it}
-      </div>
-    );
-    return elem;
-  });
-
   const enqueue = () => {
     const newQueue = [...queue];
     newQueue.push(task);
@@ -24,6 +14,8 @@ const Queue = () => {
   };
 
   const dequeue = () => {
+    if (queue.length === 0) return;
+
     const newQueue = [...queue];
     newQueue.shift();
     setQueue(newQueue);
@@ -32,17 +24,36 @@ const Queue = () => {
   return (
     <div className={styles.container}>
       <h1>Queue</h1>
-      <button onClick={enqueue}>Enqueue</button>
-      <button onClick={dequeue}>Dequeue</button>
-      <GenericEnumBasedSelect
-        _enum={Task}
-        changeHandler={(newOption: string) => setTask(newOption as Task)}
-        defaultValue={task}
-      />
+      <div className={styles.button_container}>
+        <button className={styles.button} onClick={enqueue}>
+          Enqueue
+        </button>
+        <button className={styles.button} onClick={dequeue}>
+          Dequeue
+        </button>
+      </div>
+      <div className={styles.select_container}>
+        <GenericEnumBasedSelect
+          _enum={Task}
+          changeHandler={(newOption: string) => setTask(newOption as Task)}
+          defaultValue={task}
+        />
+      </div>
       <div>
-        <div>in queue</div>
-        <div>{queueElem}</div>
-        <div>out queue</div>
+        <div className={styles.queue_status}>Out Queue</div>
+        <div className={styles.queue_item_container}>
+          {queue.map((item, index) => (
+            <div
+              key={index}
+              className={`${styles.queue_item} ${
+                index === queue.length - 1 ? styles.inserted : ""
+              }`}
+            >
+              {item} {/* Display the task directly */}
+            </div>
+          ))}
+        </div>
+        <div className={styles.queue_status}>In Queue</div>
       </div>
     </div>
   );
